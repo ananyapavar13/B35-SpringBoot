336939ea72653da9961de0a3f7392e804a51b4a6
91797 <91797@LAPTOP-I2OTCL33>
2023-02-20 13:52:01 +0530

    first commit

---
+++ springwebapp/src/main/java/org/tnsif/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,16 @@
+package org.tnsif.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+		int x=15;
+		System.out.println(x);
+	}
+
+}
